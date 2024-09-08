362b37dce09a2822ff0180190db76b4800cde045
Edmund Baker <edbaker@Edmunds-Air.local>
2024-09-08 07:41:44 +0200

    app push

--- src_shiny_app/app.py
+++ src_shiny_app/app.py
@@ -17,8 +17,9 @@ app_ui = ui.page_fluid(
             ui.h2("Page Charts"),
             ui.hr(),
             ui.input_slider(id="slider", label="Number of bins:", min=5, max=25, value=15),
-            ui.output_plot(id="histogram")
+            #ui.output_plot(id="histogram")
         ),
+        ui.output_plot(id="histogram")
     )
 )
 
