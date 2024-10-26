007016eb72e1296c61953c19a929ba3c66a8c3d6
Edmund Baker <edbaker@Edmunds-Air.local>
2024-10-26 14:27:58 +0200

    added download buttons

--- src_shiny_app/app.py
+++ src_shiny_app/app.py
@@ -9,7 +9,7 @@ Created on Sun Sep  8 06:59:49 2024
 print("starting app")
 
 from shiny import ui, render, App, reactive
-from PlotlyPlot1 import createCountryBubbleGraph  # Import the function from the other file
+from .PlotlyPlot1 import createCountryBubbleGraph  # Import the function from the other file
 import time
 # import asyncio
 import os
